5b97c9f80c8de03b43e2708b4c99e99297994454
userDiego <diegohauptman@gmail.com>
2018-08-14 14:24:22 +0200

    commit

--- src/main/java/com/run4sky/queries/GenericDAO.java
+++ src/main/java/com/run4sky/queries/GenericDAO.java
@@ -13,8 +13,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import com.run4sky.beans.BaseEntity;
-
 import javassist.NotFoundException;
 
 public class GenericDAO {
